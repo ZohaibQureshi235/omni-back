9b8af6f61634da8dd4fa23efe820f31d6012ea26
Zohaib Qureshi <126648515+ZohaibQureshi235@users.noreply.github.com>
2025-04-20 21:45:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import connectDB from './config/connectdb.js'
 
 const app = express()
 app.use(express.json({ limit: '50mb' }))
+connectDB()
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 const port = 8000
 app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001'], methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true }))
