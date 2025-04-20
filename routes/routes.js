0348be255c2f0111d0aaf8c1f8c93396bda97611
Zohaib Qureshi <126648515+ZohaibQureshi235@users.noreply.github.com>
2025-04-20 20:06:10 +0500

    adding route

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { PostImage, GetImage, updateImageViews, updateImagedowload, updateImageshare, searchImage } = require('../controller/ImageController')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+	res.send('Hello There')
+})
+
+router.post('/api/post-image', PostImage)
+router.get('/api/get-images', GetImage)
+router.post('/api/views/:imageId', updateImageViews)
+router.post('/api/download/:imageId', updateImagedowload)
+router.post('/api/share/:imageId', updateImageshare)
+router.post('/api/search/:searchTerms', searchImage)
+
+module.exports = router
