9848ff5dcb4d54081a1acca21c0f3164b09740f1
Zohaib Qureshi <126648515+ZohaibQureshi235@users.noreply.github.com>
2025-04-29 14:05:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import routes from './src/routes.js'
 import connectDB from './config/connectdb.js'
 
 const app = express()
-app.use(express.json({ limit: '10mb' }))
-app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 connectDB()
 const port = 8000
@@ -13,7 +13,6 @@ app.use(
 	cors({
 		origin: ['http://localhost:3000', 'https://omni-verse-y9my.vercel.app', 'https://omni-back-pearl.vercel.app', 'https://zohaib-qureshi235-wall-paper-qureshi-admin-panel-adsense.vercel.app'],
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		allowedHeaders: ['Content-Type'],
 		credentials: true
 	})
 )
